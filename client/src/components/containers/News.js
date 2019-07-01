19caa93d37e08a5a40d37443a315d946d4919a03
jtosborne129 <jtosborne129@gmail.com>
2019-07-01 09:11:37 -0400

    Created News Items as a Component

--- client/src/components/containers/News.js
+++ client/src/components/containers/News.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import NewsItemListing from '../presentation/NewsItemListing';
 
 export default class News extends Component {
     render() {
         return (
             <div>
-                News Items
+                <h2>News Items</h2>
+                <ul>
+                    <li><NewsItemListing id="1" title="Mad owl torments drivers" teaser= "An owl has been seen chasing cars in Bridlington" /></li>
+                    <li><NewsItemListing id="2" title="Owl steals post" teaser="Letters have gone missing in Wigan. Owl suspected." /></li>
+                </ul>
             </div>
         );
     }
